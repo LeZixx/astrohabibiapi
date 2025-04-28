291e324080f2405851be1bc570d7aa5a6a1eb196
LeZixx <talge.ziad@gmail.com>
2025-04-29 00:11:09 +0800

    fix: utils/astrology.js

--- utils/astrology.js
+++ utils/astrology.js
@@ -50,9 +50,4 @@ function calculateFullChart(julianDay, lat, lon) {
   return { ascendant, houses, planets: planetPositions };
 }
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
-
 module.exports = { calcJulianDayAndCoords, calculateFullChart };
